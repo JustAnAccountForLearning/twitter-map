5618f2710a9142f09e3b9bd18f05d6f8670c69cb
Thomas Wood <thomas.j.wood28@gmail.com>
2019-03-22 03:22:56 +0000

    add media folder with .js

--- projectsite/application/views.py
+++ projectsite/application/views.py
@@ -1,22 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Question
+from .models import Twitter
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('application/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    # Unused at the moment. It orders ALL of the tweets by time stamp
+    tweet_list = Twitter.objects.order_by('timestamp')
+    # Super short list for now. Only allows #trump and #sanders
+    hashtag_list = ['trump', 'sanders']
+    context = {'hashtag_list': hashtag_list}
+    return render(request, 'application/index.html', context)
     
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-    
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-    
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
