06d2b5845cc0604f3cd950aa5a028363bce754f6
Thomas Wood <thomas.j.wood28@gmail.com>
2019-03-27 00:48:36 +0000

    fix issue requiring user to add /application to web address

--- projectsite/application/static/application/index.js
+++ projectsite/application/static/application/index.js
@@ -1,10 +1,6 @@
-
-// TODO: Need to import jquery
-
 $(document).ready(function() {
    let canvas = document.getElementById("map_canvas");
    let ctx = canvas.getContext("2d");
    ctx.fillStyle = "#000000";
    ctx.fillRect(0,0,250,200);
-   
 });
--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('application/', include('application.urls')),
+    path('', include('application.urls')),
     path('admin/', admin.site.urls),
 ]
