17bf1aaf4c9b196e01fe919856bab722b0283368
Thomas Wood <thomas.j.wood28@gmail.com>
2019-03-27 00:29:12 +0000

    have working templates and jquery

--- projectsite/application/static/application/index.js
+++ projectsite/application/static/application/index.js
@@ -4,7 +4,7 @@
 $(document).ready(function() {
    let canvas = document.getElementById("map_canvas");
    let ctx = canvas.getContext("2d");
-   ctx.fillStyle = "#FF0000";
+   ctx.fillStyle = "#000000";
    ctx.fillRect(0,0,250,200);
    
 });
