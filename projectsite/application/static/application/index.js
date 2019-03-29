e11c03ffc71b9cb39cb05674a942266f8c01f323
Thomas Wood <thomas.j.wood28@gmail.com>
2019-03-29 13:32:06 +0000

    update index.js

--- projectsite/application/static/application/index.js
+++ projectsite/application/static/application/index.js
@@ -1,23 +1,58 @@
 $(document).ready(function() {
-   // Find the canvas for map_canvas and find context.
-   let canvas = document.getElementById("map_canvas");
-   let ctx = canvas.getContext("2d");
-   
-   // Find form and submit button
-   let form = document.getElementById("form");
-   
-   // Fit canvas to window
-   canvas.width = window.innerWidth;
-   canvas.height = window.innerHeight;
-   
-   // Fill it with a black square for now so we know where is is.
-   // Filling it from 10-90% height and width
-   ctx.fillStyle = "#000000";
-   ctx.fillRect(canvas.width*.1, canvas.height*.05, canvas.width*.8, canvas.height*.8);
-   
-   // Upon form submission.
-   form.onsubmit = function() {
-      // TODO: Validate that a new hashtag has been selected.
-      // TODO: Send AJAX request with the given hashtag.
-   };
+
+   drawMap('static/application/trump_geoJson.json');
 });
+
+
+function updateMap() {
+   let selected = document.getElementById("select_tag").value;
+   
+   console.log(selected);
+   
+   let sendData = {'hashtag': selected };
+   
+   $.getJSON("/findtweets", sendData, function(data, textStatus, jqXHR) {
+      // The data that gets returned should be a json object.
+      // Hopefully with all the tweet geo locations
+      
+      console.log(data);
+   });
+   
+   svg.selectAll("*").remove();
+   drawMap('static/application/trump_geoJson.json');
+   return false;
+}
+
+function drawMap(tweetgeo) {
+   let width = 960,
+      height = 500;
+   let svg = d3.select("body")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+   let g = svg.append("g");
+   let projection = d3.geoAlbers()
+      .scale(1000)
+      .translate([width / 2, height / 2]);
+   let path = d3.geoPath()
+      .projection(projection);
+   d3.queue()
+      .defer(d3.json, 'static/application/states.json') // Load US States
+      .defer(d3.json, tweetgeo) // Load tweet lat/long data --> CHANGE IN REAL <---
+      .await(makeMyMap); // Run 'makeMyMap' when JSONs are loaded
+
+   function makeMyMap(error,states,tweets) {
+      svg.append('path')
+          .datum(topojson.feature(states, states.objects.usStates))
+          .attr('d', path)
+          .attr('class', 'states');
+      svg.selectAll('.tweets')
+          .data(tweets.features)
+          .enter()
+          .append('path')
+          .attr('d',path)
+          .attr('class', 'tweets');
+   }
+   return false;
+}
+
