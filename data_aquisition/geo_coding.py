647ca4c5330d6f4e1e3d4ebf31a90608381d4104
Maxfield Green <mgreen13@uvm.edu>
2019-03-28 21:40:43 -0400

    added functional map in maps/

--- data_aquisition/geo_coding.py
+++ data_aquisition/geo_coding.py
@@ -9,7 +9,7 @@ import geocoder
 from geotext import GeoText
 import numpy as np
 file = "locations.json"
-bingk = "AhTP2X3M0KqYyubuu3aUz8YDMtjN9Cbz11ES2h_l1Os-kDJJTGGMdAsXzjbUDs_6" 
+bingk = "AhTP2X3M0KqYyubuu3aUz8YDMtjN9Cbz11ES2h_l1Os-kDJJTGGMdAsXzjbUDs_6"
 
 # open json file containing locations
 location_data = []
@@ -17,32 +17,34 @@ with open(file, 'r') as f:
     for line in f:
         if len(line) > 1:
             location_data.append(json.loads(line))
-    
+
 # extract locations from file
 location_data = location_data[0]['data']
 locations = []
 for tweet in location_data:
     locations.append(tweet['primary_geo'])
-    
+
 # perform geo-coding to aquire lat/lng coordinate pairs from addressess
 cords = list(np.ones([len(location_data)]))
-for idx,loc in enumerate(locations):
-    cord  = geocoder.bing(loc, key = bingk)
+for idx, loc in enumerate(locations):
+    cord = geocoder.bing(loc, key=bingk)
     if cord.latlng:
         cords[idx] = [cord.latlng]
-        
+
 # write coordinates back into tweet dictionaries
-for idx,i in enumerate(cords):
+for idx, i in enumerate(cords):
     try:
         i = i[0]
     except:
         IndexError
     location_data[idx]['coordinate'] = i
-    
+
 # aggregate tweets with geo coordinates
 geo_located_tweets = []
 for tweet in location_data:
     if tweet['coordinate'] != 1:
         geo_located_tweets.append(tweet)
-        
+
 # write data to json file
+with open('cleaned_tweets.json', 'w') as fout:
+    fout.write(json.dumps(geo_located_tweets))
